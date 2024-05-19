47c0f5039935ed395200565aca7a7c50cca14c8a
SirMatGuy <123560405+SirMatGuy@users.noreply.github.com>
2024-05-19 13:56:20 +0000

    test

--- next.config.js
+++ next.config.js
@@ -1,29 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
-
-const nextConfig = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-  basePath: "/avi-docs",
-  
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+const nextConfig = {
+  ...withNextra(),
   images: {
     unoptimized: true,
   },
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: "export",
-  
-})
-
-
+}
 module.exports = nextConfig
