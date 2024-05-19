ff815421e2e52c03c3149aece7b549c6df537fcf
SirMatGuy <123560405+SirMatGuy@users.noreply.github.com>
2024-05-19 13:20:30 +0000

    peace part 2

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  
-};
 
-// const withNextra = require('nextra')({
-//   theme: 'nextra-theme-docs',
-//   themeConfig: './theme.config.tsx',
-//   ...nextConfig
-// })
+const nextConfig = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  basePath: "/avi-docs",
+})
+
 
 module.exports = nextConfig
