43ebdcc5cd355e617eb1821220fe575ad7809ba1
SirMatGuy <123560405+SirMatGuy@users.noreply.github.com>
2024-05-19 13:25:44 +0000

    test yml

--- next.config.js
+++ next.config.js
@@ -6,6 +6,23 @@ const nextConfig = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   basePath: "/avi-docs",
+  
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+  
 })
 
 
