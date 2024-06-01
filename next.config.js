75d406f1f821329e6a40b11532cd5a9f6fa4d841
SirMatGuy <123560405+SirMatGuy@users.noreply.github.com>
2024-06-01 14:52:05 +0000

    Changed config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withNextra = require("nextra")({
 
 module.exports = withNextra({
   output: 'export',
-  basePath: "/avi-docs",
+  basePath: "/fastml-docs",
   images: {
     unoptimized: true,
   },
